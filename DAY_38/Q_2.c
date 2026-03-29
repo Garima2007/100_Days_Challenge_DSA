0b62f66c2f373f1825ff08f31d9d40af819f3581
Garima Gupta <gg3738200@gmail.com>
2026-03-29 06:46:24 +0530

    Create Q_2.c

---
+++ DAY_38/Q_2.c
@@ -0,0 +1,35 @@
+239. Sliding Window Maximum
+Constraints:
+1 <= nums.length <= 105
+-104 <= nums[i] <= 104
+1 <= k <= nums.length
+  
+int* maxSlidingWindow(int* nums, int numsSize, int k, int* returnSize) {
+    if (numsSize == 0) {
+        *returnSize = 0;
+        return NULL;
+    }
+
+    int* result = (int*)malloc((numsSize - k + 1) * sizeof(int));
+    int* deque = (int*)malloc(numsSize * sizeof(int));
+    
+    int front = 0, rear = -1;
+    int idx = 0;
+
+    for (int i = 0; i < numsSize; i++) {
+
+        if (front <= rear && deque[front] <= i - k)
+            front++;
+
+        while (front <= rear && nums[deque[rear]] < nums[i])
+            rear--;
+
+        deque[++rear] = i;
+
+        if (i >= k - 1)
+            result[idx++] = nums[deque[front]];
+    }
+
+    *returnSize = idx;
+    return result;
+}
