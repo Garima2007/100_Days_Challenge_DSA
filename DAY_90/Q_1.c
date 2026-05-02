6c41d9ebbb52a23c44dd686b1d42242ddd070da8
Garima Gupta <gg3738200@gmail.com>
2026-05-02 09:57:36 +0530

    Create Q_1.c
    Implement a solution to determine the minimum time required for k painters to paint n boards of different lengths using a binary search approach.

---
+++ DAY_90/Q_1.c
@@ -0,0 +1,71 @@
+/*
+Problem Statement
+Given n boards of different lengths and k painters, each painter paints contiguous boards. Painting a unit length of board takes one unit of time.
+
+Determine the minimum time required to paint all boards.
+
+Input Format
+n k
+n space-separated integers representing board lengths
+
+Output Format
+Print the minimum time required to paint all boards.
+
+Sample Input
+4 2
+10 20 30 40
+
+Sample Output
+60
+
+Explanation
+One painter paints boards of length 10, 20, and 30 (total 60), while the other paints board of length 40.
+*/
+#include <stdio.h>
+
+int isPossible(int arr[], int n, int k, int maxTime) {
+    int painters = 1;
+    int time = 0;
+
+    for (int i = 0; i < n; i++) {
+        if (time + arr[i] <= maxTime) {
+            time += arr[i];
+        } else {
+            painters++;
+            time = arr[i];
+        }
+    }
+
+    return painters <= k;
+}
+
+int main() {
+    int n, k;
+    scanf("%d %d", &n, &k);
+
+    int arr[n];
+    int sum = 0, maxVal = 0;
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+        sum += arr[i];
+        if (arr[i] > maxVal) maxVal = arr[i];
+    }
+
+    int low = maxVal, high = sum, ans = sum;
+
+    while (low <= high) {
+        int mid = low + (high - low) / 2;
+
+        if (isPossible(arr, n, k, mid)) {
+            ans = mid;
+            high = mid - 1;
+        } else {
+            low = mid + 1;
+        }
+    }
+
+    printf("%d", ans);
+
+    return 0;
+}
