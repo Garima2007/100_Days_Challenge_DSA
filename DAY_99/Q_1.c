4232885a19382b7a8f1964d40c6cd1879e9f3522
Garima Gupta <gg3738200@gmail.com>
2026-05-02 15:56:53 +0530

    Create Q_1.c

---
+++ DAY_99/Q_1.c
@@ -0,0 +1,40 @@
+/*Problem: Given a target distance and cars’ positions & speeds, compute the number of car fleets reaching the destination.
+Sort cars by position in descending order and calculate time to reach target.*/
+#include <stdlib.h>
+
+typedef struct {
+    int pos;
+    int speed;
+} Car;
+
+int cmp(const void* a, const void* b) {
+    Car* x = (Car*)a;
+    Car* y = (Car*)b;
+    return y->pos - x->pos;
+}
+
+int carFleet(int target, int* position, int positionSize, int* speed, int speedSize) {
+    Car* cars = (Car*)malloc(positionSize * sizeof(Car));
+
+    for (int i = 0; i < positionSize; i++) {
+        cars[i].pos = position[i];
+        cars[i].speed = speed[i];
+    }
+
+    qsort(cars, positionSize, sizeof(Car), cmp);
+
+    int fleets = 0;
+    double lastTime = 0;
+
+    for (int i = 0; i < positionSize; i++) {
+        double time = (double)(target - cars[i].pos) / cars[i].speed;
+
+        if (time > lastTime) {
+            fleets++;
+            lastTime = time;
+        }
+    }
+
+    free(cars);
+    return fleets;
+}
